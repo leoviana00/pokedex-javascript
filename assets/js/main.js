e65b68699a0a8aec1b87f04f2d745395118ea207
Leonardo Vian Pereira <leonardoviana00@hotmail.com>
2024-10-31 17:40:41 -0300

    feat(pokedex): listando os pokemons com paginacao

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,47 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
 
-const offset = 0
+const maxRecords = 151
 const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?${offset}=0&limit=${limit}`
+let offset = 0;
 
-function convertPokemonToLi(pokemon){
+function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon">
-            <span class="number">#001</span>
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
-            
+
             <div class="detail">
                 <ol class="types">
-                    <li class="type">grass</li>
-                    <li class="type">poison</li>
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
 
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}" >
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
             </div>
         </li>
     `
 }
 
-const pokemonList = document.getElementById('pokemonList')
-
-fetch(url)
-    //Encadeamento de thens (Sempre o que vai para o segundo then, é o return do primeiro e o que vai para o primeiro é a promise da fetch url)
-    .then((response) => response.json()) //TRansformando o response em uma promessa do body convertido em json
-    .then((jsonBody) => jsonBody.results) //Recebendo o body convertido e printando ele.
-    .then((pokemons) => {
-        
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)
-
-            
-        }
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML += newHtml
     })
-    .catch((error) => console.error(error))
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNexPage = offset + limit
+
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
